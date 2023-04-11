57327d238c34d915e10f7488fb302d96146af0a8
Somnath dubey <somnath.dubey9600@gmail.com>
2023-04-11 16:58:04 +0530

    About us index done!

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import AboutUs  from "../../components/AboutComponents/AboutUs";
+import AffiliationCurriculum  from "../../components/AboutComponents/AffiliationCurriculum";
+import AboutCards  from "../../components/AboutComponents/AboutCards";
 
 const index = () => {
   return (
     <>
         <AboutUs/>
+        <AffiliationCurriculum/>
+        <AboutCards/>
     </>
   )
 }
