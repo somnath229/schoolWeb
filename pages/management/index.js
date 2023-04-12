cef01f74dd3a68346d93346f63c8761be0fd71de
Somnath dubey <somnath.dubey9600@gmail.com>
2023-04-12 13:03:03 +0530

    MANAGEMENT page done

---
+++ pages/management/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ManagementCard from "../../components/ManagementComponents/ManagementCard";
+
+const index = () => {
+  return (
+    <>
+       <ManagementCard />
+    </>
+  )
+}
+
+export default index
