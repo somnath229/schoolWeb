c437d4878e4deb3ada09b498dcc1396545f7731a
Somnath dubey <somnath.dubey9600@gmail.com>
2023-04-12 11:53:35 +0530

    Done Admission page

---
+++ pages/admission/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AdmissionBg from "../../components/MainComponents/AdmissionBg";
+import AdmissionForm from "../../components/AdmissionComponents/AdmissionForm";
+
+
+const index = () => {
+  return (
+    <>
+      <AdmissionBg />
+      <AdmissionForm />
+    </>
+  )
+}
+
+export default index
