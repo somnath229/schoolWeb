3f7831c15154f85d59c8188d433f12c99155007f
Trideep-Sardar <trideepsardar@gmail.com>
2023-04-13 15:38:42 +0530

    api key, demo project id added in form api

--- utils/api/Api.js
+++ utils/api/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import moment from "moment/moment";
-const project = "";
+const project = "5f2d2d04-7afc-4ad6-8b25-1a9ca4bb08bb";
 const url = process.env.NEXT_PUBLIC_API_BASE_URL;
-const lead_source = "Website";
+const lead_source = "School Website";
 const getCommonHeaders = () => {
   let headers = {};
   headers["X_TIMESTAMP"] = Math.round(new Date() / 1000);
@@ -10,8 +10,8 @@ const getCommonHeaders = () => {
   headers["X_APPID"] = "1";
   headers["X_APPVERSION"] = "1.0.0";
   headers["X_DEVICEID"] = "1";
-  headers["X_DEVICEOS"] = "web"; 
-  headers["X_APIKEY"] = "";
+  headers["X_DEVICEOS"] = "web";
+  headers["X_APIKEY"] = "81d8b52a129aa7b2ee86b255966ebdee";
   return headers;
 };
 const normalFieldList = ["name", "phone", "email", "city"];
